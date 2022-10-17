6d91d85b1e7023886604e497df9a01856b59ea1f
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-10-17 17:55:32 +0800

    membuat namespace dan class program

--- Quickshort/Program.cs
+++ Quickshort/Program.cs
@@ -2,6 +2,14 @@
 {
     class Program
     {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0;
+
+        // number of elements in array
+        private int n;
+
 
     }
 }
