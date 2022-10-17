c21feeefe7d38d05c158d53a08bbb70e012f65cc
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-10-17 18:47:57 +0800

    membuat method display

--- Quickshort/Program.cs
+++ Quickshort/Program.cs
@@ -102,6 +102,19 @@ namespace QuickSort
             //sort the list on the right of pivot using quick sort
             q_sort(j + 1, high);
         }
+        void display()
+        {
+            Console.WriteLine("\n---------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-----------------------");
 
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparisons" + cmp_count);
+            Console.WriteLine("\nNumber of data movemenets: " + mov_count);
+        }
+        
     }
 }
