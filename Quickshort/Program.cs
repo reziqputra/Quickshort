d685377d0939b2d6f930a4a218a1753ad707cec4
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-10-17 18:03:52 +0800

    membuat method void read

--- Quickshort/Program.cs
+++ Quickshort/Program.cs
@@ -10,6 +10,29 @@
         // number of elements in array
         private int n;
 
+        void read()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n < 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
 
+            //get array elements
+            for(int i =0;i < n;i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
